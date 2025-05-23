66a4e2d929ad995bfd0408e804e31a8c730679f5
JerryRivera609 <jerry609rivera@gmail.com>
2025-05-22 22:34:25 -0500

    Proyecto travelaja

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Visa';
+
+  origen: string = '';
+  destino: string = '';
+  mensaje: string = '';
+
+  solicitar() {
+    if (this.origen && this.destino ) {
+      this.mensaje = `Solicitud de "${this.origen}" a "${this.destino}" guardada`;
+    } else {
+      this.mensaje = 'Por favor introduce origen y destino.';
+    }
+  }
 }
